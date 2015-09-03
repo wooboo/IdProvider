f271a15789b2942bf072ecf6d18e48d0795c0611
WooBoo <wooboox@gmail.com>
2015-09-03 07:23:22 +0200

    initial setup

---
+++ IdManager/Migrations/OperationalConfiguration/201504070214592_v2_0_0.cs
@@ -0,0 +1,15 @@
+using System.Data.Entity.Migrations;
+
+namespace IdManager.Migrations.OperationalConfiguration
+{
+    public partial class v2_0_0 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ WebHost/IdSvr/Factory.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * Copyright 2014 Dominick Baier, Brock Allen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IdentityServer3.Core.Configuration;
+using IdentityServer3.Core.Models;
+using IdentityServer3.Core.Services;
+using IdentityServer3.Core.Services.InMemory;
+using IdentityServer3.Core.Services.Default;
+using IdentityServer3.EntityFramework;
+
+namespace WebHost.IdSvr
+{
+    class Factory
+    {
+        public static IdentityServerServiceFactory Configure(string connString)
+        {
+            var efConfig = new EntityFrameworkServiceOptions
+            {
+                ConnectionString = connString,
+            };
+            ConfigureClients(Clients.Get(), efConfig);
+            ConfigureScopes(Scopes.Get(), efConfig);
+            var factory = new IdentityServerServiceFactory();
+
+            factory.RegisterConfigurationServices(efConfig);
+            factory.RegisterOperationalServices(efConfig);
+            factory.CorsPolicyService = new Registration<ICorsPolicyService>(new DefaultCorsPolicyService { AllowAll = true });
+            factory.ConfigureCustomUserService(connString);
+            return factory;
+        }
+        public static void ConfigureClients(IEnumerable<Client> clients, EntityFrameworkServiceOptions options)
+        {
+            using (var db = new ClientConfigurationDbContext(options.ConnectionString, options.Schema))
+            {
+                foreach (var c in clients)
+                {
+                    if (!db.Clients.Any(o => o.ClientName == c.ClientName))
+                    {
+                        var e = c.ToEntity();
+                        db.Clients.Add(e);
+                    }
+                }
+                db.SaveChanges();
+            }
+        }
+
+        public static void ConfigureScopes(IEnumerable<Scope> scopes, EntityFrameworkServiceOptions options)
+        {
+            using (var db = new ScopeConfigurationDbContext(options.ConnectionString, options.Schema))
+            {
+
+                foreach (var s in scopes)
+                {
+                    if (!db.Scopes.Any(o => o.Name == s.Name))
+                    {
+                        var e = s.ToEntity();
+                        db.Scopes.Add(e);
+                    }
+                }
+                db.SaveChanges();
+            }
+        }
+    }
+}
